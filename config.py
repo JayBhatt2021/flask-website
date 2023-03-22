60a0173f4eb579fe2cfa652d7811ad9634599275
jbhatt31 <jay.bhatt@gtri.gatech.edu>
2023-03-22 18:30:17 -0400

    Modified config.py to include default values

--- config.py
+++ config.py
@@ -15,13 +15,17 @@ class BaseConfig:
     """Sets the basic Flask configuration variables."""
 
     # Basic Configurations
-    SECRET_KEY = environ.get("SECRET_KEY")
-    SESSION_COOKIE_NAME = environ.get("SESSION_COOKIE_NAME")
+    SECRET_KEY = environ.get("SECRET_KEY", "Default Secret Key")
+    SESSION_COOKIE_NAME = environ.get(
+        "SESSION_COOKIE_NAME", "Default Session Cookie Name"
+    )
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=5)
 
     # Database Configurations
-    DATABASE_NAME = environ.get("DATABASE_NAME")
-    SQLALCHEMY_DATABASE_URI = environ.get("SQLALCHEMY_DATABASE_URI")
+    DATABASE_NAME = environ.get("DATABASE_NAME", "default.db")
+    SQLALCHEMY_DATABASE_URI = environ.get(
+        "SQLALCHEMY_DATABASE_URI", f"sqlite:///${DATABASE_NAME}"
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
